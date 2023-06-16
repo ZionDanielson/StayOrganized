a4204b015121087c8e07401f5d7836535ef55f03
Christopher Reid <chrisreid657@gmail.com>
2023-06-16 15:03:00 -0500

    added Js to todos.js

--- scripts/todos.js
+++ scripts/todos.js
@@ -2,14 +2,14 @@
 
 
 //identify variables
-usersDropdown = document.getElementById("theUsersDropdown")
-
+let usersDropdown = document.getElementById("theUsersDropdown");
+let todoTableBody = document.getElementById("usersToDosInfoTable");
 
 //wireup onload functions 
 
 window.onload = () => {
-    populateUsersDropdown();  
-    onUsersDropdownChange();
+    populateUsersDropdown();
+    usersDropdown.onchange = onUsersDropdownChange;
 }
 
 
@@ -17,37 +17,62 @@ window.onload = () => {
 
 
 //function to make dropdown (display:user's name , value:User Id)
-function populateUsersDropdown(){
+function populateUsersDropdown() {
     fetch("http://localhost:8083/api/users")
-    .then((response) => response.json())
-    .then (users => {
-       users.forEach(user=> {
-        let optionElement = document.createElement('option');
-        user.userid = optionElement.value;
-        user.name = optionElement.textContent;
-        usersDropdown.appendChild(optionElement);
-       });
-    }
-    )
+        .then((response) => response.json())
+        .then(users => {
+            users.forEach(user => {
+                let optionElement = document.createElement('option');
+                optionElement.value = user.id;
+                optionElement.textContent = user.name;
+                usersDropdown.appendChild(optionElement);
+            });
+        }
+        )
 }
 
 
 //function to get all tasks for a specific user id
-function onUsersDropdownChange(){
-    let selectedUser = usersDropdown.value
+function onUsersDropdownChange() {
+    let selectedUser = usersDropdown.value;
     console.log(selectedUser);
 
-    fetch("http://localhost:8083/api/todos")
-    .then((response) => response.json())
-    .then()
-
-    displayAllTasksPerId(selectedUser);
-
+    fetch("http://localhost:8083/api/todos/byuser/" + selectedUser)
+        .then((response) => response.json())
+        .then(todos => displayAllTodos(todos))
 
 }
 
 
-//function to display tasks for specific user id
-function displayAllTasksPerId(onUsersDropdownChange){
+//function to display a task for specific user id
+function displayAllTodos(todos) {
+
+
+
+ todoTableBody.innerHTML = "";
+
+   
+   
+    for (let todo of todos){
+        let row = todoTableBody.insertRow(-1);
+
+        let cell1 = row.insertCell(0);
+        cell1.innerHTML = todo.id;
+
+        let cell2 = row.insertCell(1);
+        cell2.innerHTML = todo.category;
+
+        let cell3 = row.insertCell(2);
+        cell3.innerHTML = todo.deadline;
+
+        let cell4 = row.insertCell(3);
+        cell4.innerHTML = todo.priority;
+
+        let cell5 = row.insertCell(4);
+        cell5.innerHTML = todo.completed;
+    
+        let cell6 = row.insertCell(5);
+        cell6.innerHTML = todo.description;
+}
 
 }
